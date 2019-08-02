bd1240121a1817939c73be234488b79d38dc40e0
xhmm <super.xhmm@gmail.com>
2019-08-02 10:31:01 +0800

    update ts rules

--- eslint/ts-base.js
+++ eslint/ts-base.js
@@ -10,18 +10,18 @@ module.exports = {
     /*--- @typescript-eslint/eslint-plugin ---*/
     // change recommended
     "@typescript-eslint/array-type": ["error", "array-simple"],
-    "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/explicit-function-return-type": ["error", {"allowExpressions": true,allowTypedFunctionExpressions: true }],
     "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/interface-name-prefix": ["error", "always"],
+    "@typescript-eslint/interface-name-prefix": ["warn", "always"],
     "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
-    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-interface": "off",
     // add custom
     "@typescript-eslint/ban-ts-ignore": ["warn"],
     "@typescript-eslint/member-ordering": ["error"],
     "@typescript-eslint/restrict-plus-operands": ["error"],
-    "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/require-await": ["error"],
     "@typescript-eslint/no-misused-promises":["error"],
 
