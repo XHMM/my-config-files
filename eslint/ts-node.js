87a05a5cb446fac721a9f2d32f54277a579a5376
xiao xin <super.xhmm@qq.com>
2020-06-12 01:19:05 +0800

    add eslint-plugin-node

--- eslint/ts-node.js
+++ eslint/ts-node.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["./ts-base.js", "plugin:security/recommended"],
+  extends: ["./ts-base.js", "plugin:security/recommended", "plugin:node/recommended"],
   plugins: ["security"],
   rules: {
     "import/no-default-export": ["error"]
