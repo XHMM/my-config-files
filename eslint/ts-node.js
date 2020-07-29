1d3b507a3eab0f70f8f504e9d78c6f30e79346a5
xiao <super.xhmm@gmail.com>
2020-07-29 09:06:11 +0800

    rm eslint-plugin-node

--- eslint/ts-node.js
+++ eslint/ts-node.js
@@ -1,10 +1,22 @@
 module.exports = {
-  extends: ["./ts-base.js", "plugin:security/recommended", "plugin:node/recommended"],
+  extends: ["./ts-base.js", "plugin:security/recommended"],
   plugins: ["security"],
   rules: {
-    "import/no-default-export": ["error"]
+    "import/no-default-export": ["error"],
+
+    /*  conflict with typescript */
+    "node/no-unsupported-features/es-syntax": "off",
+    "node/no-missing-require": "off",
+    "node/no-missing-import": "off"
   },
   env: {
     node: true
+  },
+  parserOptions: {
+    "ecmaVersion": "2020",
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "modules": true
+    }
   }
 };
